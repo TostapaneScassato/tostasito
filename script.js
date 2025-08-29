3494bdbb5952e14b2dd1180f6786dd017e770e80
TostapaneScassato <114427286+TostapaneScassato@users.noreply.github.com>
2025-08-29 14:19:51 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // dichiarazione dei pulsanti
+    const homeButton = document.getElementById("homeButton");
+    const settingsButton = document.getElementById("settingsButton");
+    const schoolButton = document.getElementById("schoolButton");
+    
+    //dove portano i movitori
+    homeButton.addEventListener("click", () => {
+        window.location.href="./homePage.html";
+    })
+    settingsButton.addEventListener("click", () => {
+        window.location.href="./workInProgress.html";
+    })
+    schoolButton.addEventListener("click", () => {
+        window.location.href="./workInProgress.html"
+    })
+})
