24800dcee4bdd90aa3dcc4f3c30e56daa08b26d9
Stefano Carletti <114427286+TostapaneScassato@users.noreply.github.com>
2025-12-03 21:23:13 +0100

    check if element exists to avoid endless errors

--- scripts/script.js
+++ scripts/script.js
@@ -177,39 +177,41 @@ document.addEventListener("DOMContentLoaded", () => {
     */
    
     let myDiv = document.getElementById("percentRemaining");
-    //Detect touch device
-    function isTouchDevice() {
-    try {
-        //We try to create TouchEvent. It would fail for desktops and throw error
-        document.createEvent("TouchEvent");
-        return true;
-    } catch (e) {
-        return false;
+    if (myDiv) {
+        //Detect touch device
+        function isTouchDevice() {
+        try {
+            //We try to create TouchEvent. It would fail for desktops and throw error
+            document.createEvent("TouchEvent");
+            return true;
+        } catch (e) {
+            return false;
+        }
+        }
+        const move = (e) => {
+        //Try, catch to avoid any errors for touch screens (Error thrown when user doesn't move his finger)
+        try {
+            //PageX and PageY return the position of client's cursor from top left of screen
+            var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+            var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+        } catch (e) {}
+        //set left and top of div based on mouse position
+        myDiv.style.left = x - 25 + "px";
+        myDiv.style.top = y - 50 + "px";
+        };
+        //For mouse
+        document.addEventListener("mousemove", (e) => {
+        move(e);
+        });
+        //For touch
+        document.addEventListener("touchmove", (e) => {
+        move(e);
+        });
+        
+        // from this point on, resumes my actual work :)
+    
+        document.getElementById("percentRemaining").innerHTML = `${percentToShow}%`;
     }
-    }
-    const move = (e) => {
-    //Try, catch to avoid any errors for touch screens (Error thrown when user doesn't move his finger)
-    try {
-        //PageX and PageY return the position of client's cursor from top left of screen
-        var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
-        var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
-    } catch (e) {}
-    //set left and top of div based on mouse position
-    myDiv.style.left = x - 25 + "px";
-    myDiv.style.top = y - 50 + "px";
-    };
-    //For mouse
-    document.addEventListener("mousemove", (e) => {
-    move(e);
-    });
-    //For touch
-    document.addEventListener("touchmove", (e) => {
-    move(e);
-    });
-
-    // from this point on, resumes my actual work :)
-
-    document.getElementById("percentRemaining").innerHTML = `${percentToShow}%`;
 
     // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - verifiche
 
