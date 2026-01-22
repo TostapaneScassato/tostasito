f30f79f349088f7052415d3178c9cbb360a13ec4
Stefano Carletti <114427286+TostapaneScassato@users.noreply.github.com>
2026-01-22 20:13:27 +0100

    school manager bugfix

--- scripts/script.js
+++ scripts/script.js
@@ -21,7 +21,7 @@ fetch("/api/me")
     });
 
 document.addEventListener("DOMContentLoaded", () => {
-    
+
     function updateThemeIcon(){
         const icon = document.querySelector("#theme-toggle i.material-icons");
         const paragraph = document.querySelector("#theme-toggle p.mainParagraph");
@@ -245,13 +245,13 @@ document.addEventListener("DOMContentLoaded", () => {
     if (myDiv) {
         //Detect touch device
         function isTouchDevice() {
-        try {
-            //We try to create TouchEvent. It would fail for desktops and throw error
-            document.createEvent("TouchEvent");
-            return true;
-        } catch (e) {
-            return false;
-        }
+            try {
+                //We try to create TouchEvent. It would fail for desktops and throw error
+                document.createEvent("TouchEvent");
+                return true;
+            } catch (e) {
+                return false;
+            }
         }
         const move = (e) => {
         //Try, catch to avoid any errors for touch screens (Error thrown when user doesn't move his finger)
